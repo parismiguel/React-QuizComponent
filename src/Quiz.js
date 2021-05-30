bea7c1a51f894aeb15c790f3aaaf852c163b624b
Paris Pantigoso <parismiguel@gmail.com>
2021-05-29 21:29:29 -0500

    Fix Question option

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component {
   render() {
     return (
       <QuizQuestion
-        quiz_question={quizData.quiz_questions[this.state.quiz_position]}
+        quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
       />
     );
   }
